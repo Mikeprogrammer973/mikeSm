8f8cbd7dc9dec7217d81621451f85e28a25a202b
Mike <antiquesclub007@gmail.com>
2025-01-16 16:40:26 -0500

    ...

--- shells/admin.py
+++ shells/admin.py
@@ -1,5 +1,4 @@
 from . import user_adm
 
 class AdminShell(user_adm.UserAdmShell):
-    intro = "Welcome to MIKESM, type 'help' or '?' to get a list of all the commands.\n"
     prompt= f"$__mikeSm__admin> "
--- shells/user_adm.py
+++ shells/user_adm.py
@@ -1,5 +1,4 @@
 from . import user
 
 class UserAdmShell(user.UserShell):
-    intro = "Welcome to MIKESM, type 'help' or '?' to get a list of all the commands.\n"
     prompt= f"$__mikeSm__user_adm> "
